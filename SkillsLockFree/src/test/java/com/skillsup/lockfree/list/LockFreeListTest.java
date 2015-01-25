5b7f745720468592eb80e97429e92fddfae87f30
dskalenko <d.skalenko@gmail.com>
2015-01-26 00:13:47 +0200

    tests

--- SkillsLockFree/src/test/java/com/skillsup/lockfree/list/LockFreeListTest.java
+++ SkillsLockFree/src/test/java/com/skillsup/lockfree/list/LockFreeListTest.java
@@ -3,29 +3,35 @@ package com.skillsup.lockfree.list;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class LockFreeListTest {
-	private final static int SIZE = 10000;
+    private final static int SIZE = 10000;
 
-	@Test
-	public void test() {
-		List<String> testList = new LockFreeListV2<>();
+    @Test
+    public void testAdd() throws NoSuchFieldException, IllegalAccessException {
+        List<String> testList = new LockFreeList<>();
+        AtomicReferenceArray<AtomicReferenceArray<String>> memory = (AtomicReferenceArray<AtomicReferenceArray<String>>) getValue(testList, "memory");
+        Assert.assertEquals(memory.length(), 32);
 		String testString = null;
-		for (int counter = 0; counter < SIZE; counter++) {
+		for (int counter = 0; counter < 1_000_000; counter++) {
 			testString = "Test + " + counter;
 			testList.add(testString);
 			Assert.assertEquals(testString, testList.get(counter));
 			Assert.assertEquals(counter + 1, testList.size());
 		}
-		Assert.assertEquals(SIZE, testList.size());
-		Assert.assertEquals("Test + 50", testList.set(50, testString));
-		Assert.assertEquals(testString, testList.get(50));
-		Assert.assertEquals(SIZE, testList.size());
-	}
+    }
+
+    private Object getValue(Object obj, String value) throws NoSuchFieldException, IllegalAccessException {
+        Field f = obj.getClass().getDeclaredField(value);
+        f.setAccessible(true);
+        return f.get(obj);
+    }
 
 	/*    @Test
-	    public void test2() throws ExecutionException, InterruptedException {
+        public void test2() throws ExecutionException, InterruptedException {
 	        final List<String> testList = new LockFreeList<>();
 	        ExecutorService executorService = Executors.newCachedThreadPool();
 	        Future<String> threadOne = executorService.submit(new Callable<String>() {
